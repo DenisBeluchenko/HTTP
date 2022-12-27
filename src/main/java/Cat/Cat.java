0051d2e012424ea76b7ff4280eabd7eeb4743ed9
Denis Beluchenko <dzibis@gmail.com>
2022-12-27 23:24:13 +0700

    final

--- src/main/java/Cat/Cat.java
+++ src/main/java/Cat/Cat.java
@@ -1,7 +1,54 @@
 package Cat;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Cat {
-    public static void main(String[] args) {
-        
+    private final String id;
+    private final String text;
+    private final String type;
+    private final String user;
+    private final String upvotes;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getUpvotes() {
+        return upvotes;
+    }
+
+    public Cat(
+            @JsonProperty("id") String id,
+            @JsonProperty("text") String text,
+            @JsonProperty("upvotes ") String upvotes,
+            @JsonProperty("type") String type,
+            @JsonProperty("user") String user
+
+    ) {
+        this.id = id;
+        this.text = text;
+        this.type = type;
+        this.user = user;
+        this.upvotes = upvotes;
+    }
+
+    @Override
+    public String toString() {
+        if(upvotes== null) {
+            return "";
+        }
+        return "id: " + id + "\ntext: " + text + "\ntype: " + type + "\nuser: " + user + "\nupvotes: " + upvotes+"\n";
     }
 }
---
+++ src/main/java/Cat/Main.java
@@ -0,0 +1,40 @@
+package Cat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class Main {
+    public static void main(String... args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        httpClient.close();
+        ObjectMapper mapper = new ObjectMapper();
+        List<Cat> posts = mapper.readValue(body, new TypeReference<>() {
+        });
+        posts.forEach(System.out::println);
+    }
+}
